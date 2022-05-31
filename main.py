e0533294308e38a5baadb883c6f7e6c04f4be04b
paul-michelle <paulmichellegallery@gmail.com>
2022-05-31 13:17:50 +0300

    Add check for duplicated drop points.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import re
-from typing import Any, Tuple, List
+from collections import Counter
+from typing import Any, Tuple, List, Optional
 
 SINGLE_DIGIT_X_n_Y_PATTERN = r"^((?P<plot_size>\dx\d)(?P<drop_points>(\(\d,\d\))*))$"
 
+Point: Tuple[int, int]
 
-def drop_points_string_to_tuples(drop_points_string: str) -> List[Tuple[int, int]]:
+
+def drop_points_string_to_tuples(drop_points_string: str) -> List['Point']:
     drop_points_string_coordinates_pairs: List[str] = drop_points_string.replace("(", "").replace(")", " ").split(" ")
 
     drop_points = []
@@ -17,8 +20,15 @@ def drop_points_string_to_tuples(drop_points_string: str) -> List[Tuple[int, int
     return drop_points
 
 
-def find_drop_points_out_of_scope(x: int, y: int, drop_points: List[Tuple[int, int]]):
-    return list(filter(lambda coord_pair: (coord_pair[0] > x or coord_pair[1] > y), drop_points))
+def find_drop_points_out_of_scope(x: int, y: int, points: List[Tuple[int, int]]) -> List[Optional['Point']]:
+    return list(filter(lambda coord_pair: (coord_pair[0] > x or coord_pair[1] > y), points))
+
+
+def find_duplicated_drop_points(drop_points: List['Point']) -> Tuple[List[Optional['Point']], List['Point']]:
+    unique_drop_points = list(set(drop_points))
+    if len(unique_drop_points) < len(drop_points):
+        duplicates = list((Counter(drop_points) - Counter(unique_drop_points)).elements())
+        return duplicates, unique_drop_points
 
 
 def parse_plot_description(plot_descr: str, pattern: str = SINGLE_DIGIT_X_n_Y_PATTERN) -> Any:
@@ -43,12 +53,16 @@ def parse_plot_description(plot_descr: str, pattern: str = SINGLE_DIGIT_X_n_Y_PA
         return
     drop_points: List[Tuple[int, int]] = drop_points_string_to_tuples(drop_points_string)
 
-    drop_points_out_of_scope = find_drop_points_out_of_scope(plot_x_size, plot_y_size, drop_points)
+    duplicated_drop_points, unique_drop_points = find_duplicated_drop_points(drop_points)
+    if duplicated_drop_points:
+        print(f'Warning: duplicated drop point(s) found: {duplicated_drop_points}, and will not be considered.')
+
+    drop_points_out_of_scope = find_drop_points_out_of_scope(plot_x_size, plot_y_size, unique_drop_points)
     if drop_points_out_of_scope:
         print(f'Drop point(s) situated out of specified scope: {drop_points_out_of_scope}')
         return
 
-    print(plot_x_size, plot_y_size, drop_points)
+    print(plot_x_size, plot_y_size, unique_drop_points)
 
 
 if __name__ == '__main__':
